8b068a80d17a5cd82a33d5f92e71c215ce6503d7
LSLegend24 <160636682+LSLCOder@users.noreply.github.com>
2024-05-13 17:44:47 +0800

    2

--- database.php
+++ database.php
@@ -5,4 +5,8 @@ $password ='';
 $database='filebase';
 $port='3306';
 $connection = mysqli_connect($host,$user,$password,$database,$port);
+
+if ($connection->connect_error) {
+    die ("Connection failed: " . $connection->connect_error);
+}
 ?>
